d3bbc351a10635971d9d10cc81c72264421a9196
Aarish-PC <aarishalam22@gmail.com>
2024-07-01 23:08:08 +0530

    added quicksort

---
+++ Sorting Algos/QuickSort.py
@@ -0,0 +1,13 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr)//2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+
+arr = [87,54,7,88,67]
+sorted_arr = quick_sort(arr)
+print(sorted_arr)
